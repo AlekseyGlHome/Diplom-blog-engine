52b230702a75854a11c02e6655be298d244112b6
AlekseyGl <glushenko78@mail.ru>
2022-04-25 17:00:32 +0300

    fix api/profile/my

--- src/main/java/com/skillbox/engine/api/request/Profile.java
+++ src/main/java/com/skillbox/engine/api/request/Profile.java
@@ -5,11 +5,11 @@ import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Profile {
     private String name;
     private String email;
     private String password;
     private Integer removePhoto;
-    private MultipartFile photo;
+    private String photo;
 }
--- src/main/java/com/skillbox/engine/controller/ApiProfile.java
+++ src/main/java/com/skillbox/engine/controller/ApiProfile.java
@@ -4,22 +4,29 @@ import com.skillbox.engine.api.request.Profile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/profile")
 @RequiredArgsConstructor
 public class ApiProfile {
 
+
     @PostMapping(value = "/my", consumes = "application/json")
     @PreAuthorize("hasAuthority('user:moderate')||hasAuthority('user:write')")
-    public String MyWithoutPhoto(@RequestBody Profile profile) {
+    public String MyWithoutPhoto(
+            @RequestBody(required = false) Profile profile) {
 
         return "";
     }
 
     @PostMapping(value = "/my", consumes = "multipart/form-data")
     @PreAuthorize("hasAuthority('user:moderate')||hasAuthority('user:write')")
-    public String MyWithPhoto(@RequestParam Profile profile) {
+    public String MyWithPhoto(@RequestParam(required = false) MultipartFile photo,
+                              @RequestParam(required = false) Integer removePhoto,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email,
+                              @RequestParam(required = false) String password) {
 
         return "";
     }
